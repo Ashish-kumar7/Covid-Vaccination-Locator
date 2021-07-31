d1c771c1d88ca6dac00f00295d4551adf86b5c14
ashish007kumar <ashishkumar357ak@gmail.com>
2021-07-31 17:45:25 +0530

    added

--- javascript.js
+++ javascript.js
@@ -24,14 +24,18 @@ function searchfunc() {
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             // Success!
+            console.log(request.status);
             var data = JSON.parse(request.responseText);
             let dataarray = data.Hospitaldetails;
 
             let totdetails = "";
+            var flag=0;
             dataarray.forEach(function (element, index) {
                 inputdata = inputdata.toLowerCase();
+                flag=flag+1;
                 if (element.District === (inputdata) || element.District === (inputdata.toUpperCase()) ||
                     element.District === (inputdata.toLowerCase()) || element.District === (capitalizeFirstLetter(inputdata))) {
+                        // flag=0;
                     let details = `
                     <div class="card">
                     <div class="card-header" id="heading${index}">
@@ -60,10 +64,17 @@ function searchfunc() {
                     totdetails += details;
                     accordion.innerHTML = totdetails;
                 }
+                else if(flag>7935){
+                    console.log(flag);
+                    alert("The given City is Not found . Please check the city name again!!");
+                    flag=0;
+                }
             });
+            
+        
         } else {
             // We reached our target server, but it returned an error
-            alert("error");
+            alert("Error");
         }
     };
     request.send();
